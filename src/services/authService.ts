3327a107d227d2ffaa4863f95ff6d3e87c272dbc
thaisealves <alves.thaisesilva@gmail.com>
2022-10-05 14:12:44 -0300

    feat: adding the password hashed

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,14 +1,14 @@
 import { createUserRepository } from "../repositories/authRepository";
 import { CreateUserType } from "../types/authTypes";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 async function createNewUser(newUser: CreateUserType) {
-//   const formatedUser = {
-//     ...newUser,
-//     password: bcrypt.hashSync(newUser.password, 10),
-//   };
+  const formatedUser = {
+    ...newUser,
+    password: bcrypt.hashSync(newUser.password, 10),
+  };
 
-  await createUserRepository(newUser);
+  await createUserRepository(formatedUser);
 }
 
 export { createNewUser };

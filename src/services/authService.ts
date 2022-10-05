8a701088f367de3d6257689833a464ca9b3948ee
thaisealves <alves.thaisesilva@gmail.com>
2022-10-05 15:29:59 -0300

    feat: adding a service to verify email and password

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,7 +2,7 @@ import {
   createUserRepository,
   findUserByEmail,
 } from "../repositories/authRepository";
-import { CreateUserType } from "../types/authTypes";
+import { CreateUserType, SignInUserType } from "../types/authTypes";
 import bcrypt from "bcrypt";
 
 async function createNewUser(newUser: CreateUserType) {
@@ -12,7 +12,7 @@ async function createNewUser(newUser: CreateUserType) {
     ...newUser,
     password: bcrypt.hashSync(newUser.password, 10),
   };
-  
+
   if (existingUser) {
     throw { code: "Conflict", message: "Email already in use" };
   }
@@ -20,4 +20,18 @@ async function createNewUser(newUser: CreateUserType) {
   await createUserRepository(formatedUser);
 }
 
+async function loginService(user: SignInUserType) {
+  const existingUser = await findUserByEmail(user.email);
+
+  if (!existingUser) {
+    throw { code: "Conflict", message: "User doesn't exists" };
+  }
+  verifyUser(user.password, existingUser.password);
+}
+
+function verifyUser(givenPass: string, originalPass: string) {
+  if (!bcrypt.compareSync(givenPass, originalPass)) {
+    throw { code: "Unauthorized", message: "Data doesn't match!" };
+  }
+}
 export { createNewUser };

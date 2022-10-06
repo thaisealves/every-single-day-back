e9f580f7ab0926e1ee635dd82f490e052a831f06
thaisealves <alves.thaisesilva@gmail.com>
2022-10-05 23:41:38 -0300

    feat: adding a validate token middleware for all the routes excluding the login and signup

--- src/routes/routesIndex.ts
+++ src/routes/routesIndex.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { validateToken } from "../middlewares/validateToken";
 import authRouter from "./authRouter";
+
 const route = Router();
 route.use(authRouter);
+route.use(validateToken)
 export default route;

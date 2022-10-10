228563cba9a2ee0752c95ffe896c8ad61f77b42c
thaisealves <alves.thaisesilva@gmail.com>
2022-10-10 11:49:17 -0300

    test: integration test for water

--- test/integration/water.test.ts
+++ test/integration/water.test.ts
@@ -5,70 +5,48 @@ import { prisma } from "../../src/utils/database";
 import { tokenFactory } from "../factories/tokenFactory";
 
 beforeEach(async () => {
-  await prisma.$executeRaw`TRUNCATE TABLE "diary" CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "water" CASCADE`;
 });
-describe("Testing /POST on diary", () => {
+describe("Testing /POST on water", () => {
   it("Must return 201 if the body is correct", async () => {
     const token = await tokenFactory();
     const body = {
-      type: "plans",
       createdAt: "22-09-2011",
-      text: faker.lorem.paragraph(),
+      waterQuantity: faker.datatype.number(5),
     };
 
     const creating = await supertest(app)
-      .post("/diary")
+      .post("/water")
       .send(body)
       .set({ Authorization: `Bearer ${token.body.token}` });
 
     expect(creating.status).toBe(201);
   });
-  it("Must return 409 if diary type already exists", async () => {
-    const token = await tokenFactory();
-    const body = {
-      type: "plans",
-      createdAt: "22-09-2011",
-      text: faker.lorem.paragraph(),
-    };
-
-    await supertest(app)
-      .post("/diary")
-      .send(body)
-      .set({ Authorization: `Bearer ${token.body.token}` });
-    const creating = await supertest(app)
-      .post("/diary")
-      .send(body)
-      .set({ Authorization: `Bearer ${token.body.token}` });
-
-    expect(creating.status).toBe(409);
-  });
 });
-describe("Testing /GET on diary/:day", () => {
+describe("Testing /GET on water/:day", () => {
   it("Must return 200 and an array of diaries, with length > 0", async () => {
     const token = await tokenFactory();
     const body = {
-      type: "plans",
       createdAt: "22-09-2011",
-      text: faker.lorem.paragraph(),
+      waterQuantity: faker.datatype.number(5),
     };
     await supertest(app)
-      .post("/diary")
+      .post("/water")
       .send(body)
       .set({ Authorization: `Bearer ${token.body.token}` });
     const getting = await supertest(app)
-      .get(`/diary/${body.createdAt}`)
+      .get(`/water/${body.createdAt}`)
       .send()
       .set({ Authorization: `Bearer ${token.body.token}` });
-
     expect(getting.status).toBe(200);
-    expect(getting.body).toBeInstanceOf(Array);
-    expect(getting.body.length).toBeGreaterThan(0);
+    expect(getting.body).toBeInstanceOf(Object);
+    expect(getting.body).toHaveProperty("waterQuantity");
   });
-  it("Must return 404 if there's no diary for user that day", async () => {
+  it("Must return 404 if there's no water for user that day", async () => {
     const token = await tokenFactory();
 
     const getting = await supertest(app)
-      .get("/diary/22-09-2011")
+      .get("/water/22-09-2011")
       .send()
       .set({ Authorization: `Bearer ${token.body.token}` });
 
